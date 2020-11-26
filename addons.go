31bbfb9618ebe743e39df4713efa348f58363d94
Chris Smith <chris@chameth.com>
2020-11-26 16:37:18 +0000

    Directories should be tracked for all addon types

--- addons.go
+++ addons.go
@@ -10,24 +10,24 @@ const (
 )
 
 type BaseAddon struct {
-	Type AddonType `json:"type"`
+	Type        AddonType `json:"type"`
+	Directories []string  `json:"directories"`
 }
 
 type CurseForgeAddon struct {
 	BaseAddon
-	Id          int      `json:"id"`
-	Name        string   `json:"name"`
-	FileId      int      `json:"file_id"`
-	Directories []string `json:"directories"`
+	Id     int    `json:"id"`
+	Name   string `json:"name"`
+	FileId int    `json:"file_id"`
 }
 
 func (t AddonType) NewInstance() (*CurseForgeAddon, error) {
 	switch t {
 	case TypeUnspecified:
 		// For compatibility with old configs, if the type field is missing default to curseforge
-		return &CurseForgeAddon{BaseAddon: BaseAddon{TypeCurseForge}}, nil
+		return &CurseForgeAddon{BaseAddon: BaseAddon{Type: TypeCurseForge}}, nil
 	case TypeCurseForge:
-		return &CurseForgeAddon{BaseAddon: BaseAddon{TypeCurseForge}}, nil
+		return &CurseForgeAddon{BaseAddon: BaseAddon{Type: TypeCurseForge}}, nil
 	default:
 		return nil, fmt.Errorf("unknown addon type: %s", t)
 	}
